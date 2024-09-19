9bf1cd66fa3c94050557a770c8a5cbd2100f2ad2
Sharma7827 <sharmas4063@gmail.com>
2024-09-19 21:44:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function getFormvalue() {
-    //Write your code here
+let fname = document.querySelector(".fname")
+let lname = document.querySelector(".lname")
+let btn1 = document.querySelector(".btn")
+
+btn.addEventListener("click" , getFormvalue)
+
+function getFormvalue(e) {
+	e.preventDefault()
+	let name1 = fname.value
+	let name2 = lname.value
+
+	alert(`${name1} + ${name2}`)
+
+	
+   
 
 }
